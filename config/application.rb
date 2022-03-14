829aba3c4a1f2318ff93d6b9b3d67a4ce0e39dff
Roberto Plancarte <roberto.plancarte@apptegy.com>
2022-03-14 16:59:56 -0600

    x

---
+++ config/application.rb
@@ -0,0 +1,23 @@
+require_relative "boot"
+
+require "rails/all"
+
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(*Rails.groups)
+
+module Ale
+  class Application < Rails::Application
+    # Initialize configuration defaults for originally generated Rails version.
+    config.load_defaults 7.0
+
+    # Configuration for the application, engines, and railties goes here.
+    #
+    # These settings can be overridden in specific environments using the files
+    # in config/environments, which are processed later.
+    #
+    # config.time_zone = "Central Time (US & Canada)"
+    # config.eager_load_paths << Rails.root.join("extras")
+    config.hosts << "6fbe-2806-108e-13-381d-c822-1fd2-1224-dc26.ngrok.io"
+  end
+end
